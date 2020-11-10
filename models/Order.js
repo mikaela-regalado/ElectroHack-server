7c4cabee8f0c95d452aaa273b97d443a8831138b
Mikaela22 <61974476+Mikaela22@users.noreply.github.com>
2020-11-10 15:22:42 -0300

    models Category and Order

---
+++ models/Order.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+    {
+      buyer: {​​​​ type: Schema.Types.ObjectId, ref: "User" },
+      list_products: [ {​​​​ type: Schema.Types.ObjectId, ref: "Product" }],
+      state: String,
+        },
+    { timestamps: true }
+  ); 
+
+  modules.exports = orderSchema
