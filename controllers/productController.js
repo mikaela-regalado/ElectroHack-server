06d4df8b3b8011c91abb41d5915a9719833b36e4
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-11-12 11:02:55 -0300

    agregue funciones de orderController. Falta probar

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,7 @@
 const db = require("../models");
-const categoryController = require("./categoryController");
 
 const productController = {
-  all: async (req, res) => {
+  many: async (req, res) => {
     let query = req.query;
     if (query.slug) {
       const category = await db.Category.findOne(req.query);
