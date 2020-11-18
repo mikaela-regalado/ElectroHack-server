1727af9761dc896c1022693134000d35afa2390b
Nacho A <48703982+MateSeco@users.noreply.github.com>
2020-11-18 18:44:58 -0300

    seeder

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -1,28 +1,76 @@
 const path = require("path");
 const categories = path.resolve(__dirname, "categories.csv");
-console.log(categories);
+const products = path.resolve(__dirname, "products.csv");
+const admin = path.resolve(__dirname, "admin.csv");
 const csv = require("csvtojson");
 const db = require("../models");
 
-const seeder = () => {
-  csv()
+const seeder = async () => {
+  await csv()
+    .fromFile(admin)
+    .then((jsonObj) => {
+      jsonObj.map(async ({ firstName, lastName, email, password, isAdmin }) => {
+        const newAdmin = await new db.Admin({
+          firstName,
+          lastName,
+          email,
+          password,
+          isAdmin,
+        });
+        await newAdmin.save();
+        console.log(newAdmin);
+      });
+    });
+
+  await csv()
     .fromFile(categories)
     .then((jsonObj) => {
-      jsonObj.map(({ code, type, description, image }) => {
-        const newCategory = new db.Category({
+      jsonObj.map(async ({ code, type, description, image }) => {
+        const newCategory = await new db.Category({
           code,
           type,
           description,
           slugify: type,
-          image: `${process.env.URL_S3}categories/${image}`,
+          image: `categories/${image}`,
           productList: [],
         });
-        newCategory.save();
+        await newCategory.save();
         console.log(newCategory);
       });
+    });
+
+  await csv()
+    .fromFile(products)
+    .then((jsonObj) => {
+      jsonObj.map(
+        async ({
+          name,
+          description,
+          image,
+          price,
+          stock,
+          outstanding,
+          code,
+        }) => {
+          const product = await new db.Product({
+            name,
+            description,
+            image: `products/${image}`,
+            price,
+            stock,
+            outstanding,
+            slugify: name,
+          });
+
+          const category = await db.Category.findOne({ code: code });
+          category.productList.push(product._id);
+          await category.save();
+          product.category = category._id;
+          await product.save();
+          console.log(product);
+        }
+      );
     });
 };
 
 module.exports = seeder;
-/* // Async / await usage
-const jsonArray=await csv().fromFile(categories); */
